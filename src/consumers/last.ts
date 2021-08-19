5682b17cc9e6b746cfcddcd034d250995d96fb11
Dimitar Ruskov <dimitar@ruskov.ruskov>
2021-08-19 17:10:22 +0300

    [Fix] The predicate in the 'last()' method is optional now.

--- src/consumers/last.ts
+++ src/consumers/last.ts
@@ -1,8 +1,8 @@
-export function last<T, R, N>(iterator: Iterator<T, R, N>, predicate: (value: T) => boolean): T | undefined {
+export function last<T, R, N>(iterator: Iterator<T, R, N>, predicate?: (value: T) => boolean): T | undefined {
     let x = iterator.next();
     let result: T | undefined;
     while (x.done !== true) {
-        if (predicate(x.value)) {
+        if (predicate == null || predicate(x.value)) {
             result = x.value;
         }
         x = iterator.next();
