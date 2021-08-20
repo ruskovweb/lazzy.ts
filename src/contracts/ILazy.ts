3250e028357196296797a1d04459e4cb81cded48
Dimitar Ruskov <dimitar@ruskov.ruskov>
2021-08-20 15:33:16 +0300

    [Feature] The 'randomInt' method has been renamed to 'random' and now works with floating-point numbers.

--- src/contracts/ILazy.ts
+++ src/contracts/ILazy.ts
@@ -1,10 +1,10 @@
-import { RangeParams } from "..";
 import { ILazyCollection } from "./ILazyCollection";
+import { RandomIntParams, RangeParams } from "..";
 
 export interface ILazy {
+    circular<T>(iterable: Iterable<T>): ILazyCollection<T, undefined, undefined>;
     from: <T, R, N>(source: Iterable<T> | Iterator<T, R, N>) => ILazyCollection<T, R | undefined, N | undefined>;
     generate: <T> (func: () => T) => ILazyCollection<T, undefined, undefined>;
+    random(parameters?: Partial<RandomIntParams>): ILazyCollection<number, undefined, undefined>;
     range(parameters?: Partial<RangeParams>): ILazyCollection<number, undefined, undefined>;
-    circular<T>(iterable: Iterable<T>): ILazyCollection<T, undefined, undefined>;
-    randomInt(lessThan: number): ILazyCollection<number, undefined, undefined>;
 }
