a28835e4c39270251f1c6cf2bc5ee918eb25c7cc
Dimitar Ruskov <ruskovweb@gmail.com>
2021-08-26 14:26:57 +0300

    New function called 'fibonacci' has been added.

--- src/contracts/ILazy.ts
+++ src/contracts/ILazy.ts
@@ -4,6 +4,7 @@ import { RandomParams, RangeParams } from "..";
 export interface ILazy {
     circular<T>(iterable: Iterable<T>): ILazyCollection<T, undefined, undefined>;
     from: <T, R, N>(source: Iterable<T> | Iterator<T, R, N>) => ILazyCollection<T, R | undefined, N | undefined>;
+    fibonacci: (minimum?: number) => ILazyCollection<number, void, number>;
     generate: <T> (func: () => T) => ILazyCollection<T, undefined, undefined>;
     prime(minimum?: number): ILazyCollection<number, void, number>;
     random(parameters?: Partial<RandomParams>): ILazyCollection<number, undefined, undefined>;
